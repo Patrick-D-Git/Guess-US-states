a1bd5e05196115d0dcdf8634e601b110ffeae49b
Patrick D <patrick.montejo@gmail.com>
2024-03-03 13:48:41 -0500

    add a condition that if answer is correct, state name will appear on map

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ screen.addshape(image)
 turtle.shape(image)
 screen.setup(width=800, height=600)
 
-df = pd.read_csv("50_states.csv")
-state_list = df["state"].tolist()
+data = pd.read_csv("50_states.csv")
+state_list = data.state.to_list()
+
+answer_state = screen.textinput(title="Guess the state", prompt="What's another state name?")
+
+if answer_state in state_list:
+    s = turtle.Turtle()
+    s.hideturtle()
+    s.penup()
+    state_data = data[data.state == answer_state]
+    s.goto(int(state_data.x), int(state_data.y))
+    s.write(f"{answer_state}", align="center")
 
 screen.exitonclick()
