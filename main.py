3c6349068c71c052daf9b3fac66e6df2191820c9
Patrick D <patrick.montejo@gmail.com>
2024-03-03 13:55:30 -0500

    added a while loop to continously guess state

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ screen.setup(width=800, height=600)
 
 data = pd.read_csv("50_states.csv")
 state_list = data.state.to_list()
+guessed_state_list = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state name?")
+while len(guessed_state_list) < len(state_list):
 
-if answer_state in state_list:
-    s = turtle.Turtle()
-    s.hideturtle()
-    s.penup()
-    state_data = data[data.state == answer_state]
-    s.goto(int(state_data.x), int(state_data.y))
-    s.write(f"{answer_state}", align="center")
+    answer_state = screen.textinput(title=f"{len(guessed_state_list)}/{len(state_list)} Guessed State",
+                                    prompt="What's another state name?").title()
+
+    if answer_state in state_list and answer_state not in guessed_state_list:
+
+        s = turtle.Turtle()
+        s.hideturtle()
+        s.penup()
+        state_data = data[data.state == answer_state]
+        s.goto(int(state_data.x), int(state_data.y))
+        s.write(f"{answer_state}", align="center")
+        guessed_state_list.append(answer_state)
 
 screen.exitonclick()
